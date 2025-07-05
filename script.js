c6e435518c587f6b185a30eb4d6daf5938314cb0
LucasGoulart24 <Goulart7720@gmail.com>
2025-07-05 16:31:54 -0300

    relogio

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function updateTime() {
     const timeElement = document.getElementById('time');
+    const dataElement = document.getElementById('data');
     const now = new Date;
+
     const horas = now.getHours().toString().padStart(2,'0');
     const minutos = now.getMinutes().toString().padStart(2,'0');
     const secundos = now.getSeconds().toString().padStart(2,'0');
+    
+    const dia = now.getDate().toString().padStart(2, '0');
+    const mes = (now.getMonth() + 1).toString().padStart(2, '0');
+    const ano = now.getFullYear();
 
     const timeString = `${horas}:${minutos}:${secundos}`;
+    const dateString = `${dia}/${mes}/${ano}`;
     timeElement.textContent = timeString;
+    dataElement.textContent = dateString;
 }
 
 setInterval(updateTime, 1000);
